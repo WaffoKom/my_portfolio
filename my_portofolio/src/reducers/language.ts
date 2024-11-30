e696576fb23d94de18f53e3848d9bbe8c260b175
DaniloWaffis <danilowaffis@gmail.com>
2024-11-30 18:41:56 +0100

    language reducer and language type values

--- my_portofolio/src/reducers/language.ts
+++ my_portofolio/src/reducers/language.ts
@@ -1,3 +1,25 @@
-export default function language(state, action) {
-  return state;
+import { TranslationState } from "../types/LanguageState.ts";
+import { TranslationActionTypes } from "../actions/languageAction.ts";
+import { SET_LANGUAGE } from "../constants/actionTypes.ts";
+import i18n from "../i18next.ts";
+
+const initialState: TranslationState = {
+  currentLanguage: "en",
+};
+
+export default function language(
+  state = initialState,
+  action: TranslationActionTypes
+): TranslationState {
+  switch (action.type) {
+    case SET_LANGUAGE:
+      i18n.changeLanguage(action.payload);
+      return {
+        ...state,
+        currentLanguage: action.payload,
+      };
+
+    default:
+      return state;
+  }
 }
--- my_portofolio/src/types/LanguageState.ts
+++ my_portofolio/src/types/LanguageState.ts
@@ -1,17 +1,8 @@
 export interface TranslationState {
   currentLanguage: string;
-  translation: { [key: string]: { [key: string]: string } };
 }
 
-// exeample of object translation
-
-export const translations = {
-  en: {
-    welcome: "welcome",
-    home: "home",
-  },
-  fr: {
-    Bienvenue: "Bienvenue",
-    Accueil: "Accueil",
-  },
-};
+// export interface TranslationState {
+//   currentLanguage: string;
+//   translation: { [key: string]: { [key: string]: string } };
+// }
