5da2127a346789065356feec821d233f9b76b407
DaniloWaffis <danilowaffis@gmail.com>
2024-12-08 01:22:01 +0100

    update store

--- my_portofolio/src/store/store.ts
+++ my_portofolio/src/store/store.ts
@@ -1,7 +1,16 @@
 import { legacy_createStore as createStore, Store } from "redux";
 import reducer from "../reducers/index.js";
 import { AppState } from "../types/AppState.ts";
+import { ThemeActionTypes } from "../actions/themeActions.ts";
+import { TranslationActionTypes } from "../actions/languageAction.ts";
+import { TestimonialsActionTypes } from "../actions/testimonialAction.ts";
 
-const store: Store<AppState> = createStore(reducer);
+export type AppActions =
+  | ThemeActionTypes
+  | TranslationActionTypes
+  | TestimonialsActionTypes;
 
+const store: Store<AppState, AppActions> = createStore(reducer);
+export type RootState = ReturnType<typeof reducer>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
