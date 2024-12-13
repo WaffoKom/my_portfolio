71d8eb8ac9b46ffded578c9801832e45ec378435
DaniloWaffis <danilowaffis@gmail.com>
2024-12-13 20:59:23 +0100

    App component restructuration

--- my_portofolio/src/App.tsx
+++ my_portofolio/src/App.tsx
@@ -1,29 +1,24 @@
 import "./App.css";
+import React from "react";
 import About from "./components/home/About.tsx";
 import Skills from "./components/skills/Skills.tsx";
 import Projets from "./components/projets/Projets.tsx";
 import Career from "./components/career/Career.tsx";
+import Header from "./components/Header/Header.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "./hooks.ts";
-import { setTheme } from "./actions/themeActions.ts";
 
-export default function App() {
-  const { theme } = useAppSelector((state) => state.theme);
-  const dispatch = useAppDispatch();
-  const handleToggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    dispatch(setTheme(newTheme));
-  };
+const App: React.FC = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projets" element={<Projets />} />
-          <Route path="/career" element={<Career />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projets" element={<Projets />} />
+        <Route path="/career" element={<Career />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;
