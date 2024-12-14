7085d7917cf423854807bb90a85fa8844a16e145
DaniloWaffis <danilowaffis@gmail.com>
2024-12-14 19:27:34 +0100

    Header component with theme and language

--- my_portofolio/src/components/Header/Header.tsx
+++ my_portofolio/src/components/Header/Header.tsx
@@ -3,31 +3,58 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { setTheme } from "../../actions/themeActions";
 import { useAppSelector, useAppDispatch } from "../../hooks";
-import { Button } from "flowbite-react";
+import { Button, Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { setLanguage } from "../../actions/languageAction";
+import fr from "../../assets/lang-img/fr.png";
+import en from "../../assets/lang-img/en.png";
 
 const Header: React.FC = () => {
   const { theme } = useAppSelector((state) => state.theme);
+  const { currentLanguage } = useAppSelector((state) => state.language);
   const dispatch = useAppDispatch();
   const handleToggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     dispatch(setTheme(newTheme));
   };
 
+  const handleChangeLanguage = (newLanguage: string) => {
+    newLanguage = currentLanguage == "en" ? "fr" : "en";
+    dispatch(setLanguage(newLanguage));
+  };
+
   return (
     <div>
-      <Button
-        onClick={handleToggleTheme}
-        style={{
-          backgroundColor: `var(--buton-background)`,
-          color: `var(--button-text-color)`,
-        }}
-      >
-        {theme === "light" ? (
-          <FontAwesomeIcon icon={faMoon} />
-        ) : (
-          <FontAwesomeIcon icon={faSun} />
-        )}
-      </Button>
+      <Navbar>
+        <Link to="" />
+        <Button
+          onClick={handleToggleTheme}
+          style={{
+            backgroundColor: `var(--buton-background)`,
+            color: `var(--button-text-color)`,
+          }}
+        >
+          {theme === "light" ? (
+            <FontAwesomeIcon icon={faMoon} />
+          ) : (
+            <FontAwesomeIcon icon={faSun} />
+          )}
+        </Button>
+        <Button
+          onClick={() => {
+            handleChangeLanguage("en");
+          }}
+        >
+          <img src={en} alt="English version" />
+        </Button>
+        <Button
+          onClick={() => {
+            handleChangeLanguage("fr");
+          }}
+        >
+          <img src={fr} alt="French Version" />
+        </Button>
+      </Navbar>
     </div>
   );
 };
