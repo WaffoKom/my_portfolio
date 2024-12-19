10766e7c0fbc5a175879b35c09123fbc64394d74
DaniloWaffis <danilowaffis@gmail.com>
2024-12-19 15:30:14 +0100

    update  language config files

--- my_portofolio/src/components/skills/Skills.tsx
+++ my_portofolio/src/components/skills/Skills.tsx
@@ -1,5 +1,25 @@
 import "./Skills.css";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-export default function Skills() {
-  return <div>Skills</div>;
-}
+const Skills: React.FC = () => {
+  const { t } = useTranslation("skills");
+  return (
+    <section>
+      <div>
+        <h2>A propos de moi.</h2>
+        <p>
+          ---Développeur Full Stack Web & Mobile créatif 🚀** Passionné par
+          l'innovation et l'amélioration des interfaces utilisateurs, je conçois
+          des solutions web et mobiles intuitives et esthétiques. Que ce soit
+          sur un écran de smartphone ou de bureau, je m'assure que chaque
+          interaction soit fluide et agréable. 📱💻 Ouvert aux consultations et
+          collaborations pour des projets dynamiques et ambitieux. --- Dis-moi
+          si cela te convient ou si tu as d'autres préférences ! 😊
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
