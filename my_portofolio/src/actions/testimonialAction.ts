b784a3cb21089ba6bd48e0d34bf52bb826460de2
DaniloWaffis <danilowaffis@gmail.com>
2024-12-20 23:01:37 +0100

    commenter le code des actions de témoignages dans testimonialAction.ts

--- my_portofolio/src/actions/testimonialAction.ts
+++ my_portofolio/src/actions/testimonialAction.ts
@@ -1,18 +1,18 @@
-import { LOAD_TESTIMONIALS } from "../constants/actionTypes.ts";
-import { Testiomonial } from "../types/TestimonyState.ts";
+// import { LOAD_TESTIMONIALS } from "../constants/actionTypes.ts";
+// import { Testiomonial } from "../types/TestimonyState.ts";
 
-export interface LoadTestimonialsAction {
-  type: typeof LOAD_TESTIMONIALS;
-  payload: { [id: number]: Testiomonial };
-}
+// export interface LoadTestimonialsAction {
+//   type: typeof LOAD_TESTIMONIALS;
+//   payload: { [id: number]: Testiomonial };
+// }
 
-export type TestimonialsActionTypes = LoadTestimonialsAction;
+// export type TestimonialsActionTypes = LoadTestimonialsAction;
 
-// Action creator for Add or Delete a Testimony
+// // Action creator for Add or Delete a Testimony
 
-export const loadTestimonials = (testimonials: {
-  [id: number]: Testiomonial;
-}): TestimonialsActionTypes => ({
-  type: LOAD_TESTIMONIALS,
-  payload: testimonials,
-});
+// export const loadTestimonials = (testimonials: {
+//   [id: number]: Testiomonial;
+// }): TestimonialsActionTypes => ({
+//   type: LOAD_TESTIMONIALS,
+//   payload: testimonials,
+// });
