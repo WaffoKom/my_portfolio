45d1b5a1fa49f15a0481a7b22971dac2ca1bc52b
DaniloWaffis <danilowaffis@gmail.com>
2024-12-20 23:01:25 +0100

    commenter le réducteur de témoignages et les types associés dans testimony.ts

--- my_portofolio/src/reducers/index.ts
+++ my_portofolio/src/reducers/index.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import language from "./language.js";
-import testimony from "./testimony.js";
+// import testimony from "./testimony.js";
 import theme from "./theme.js";
 
 export default combineReducers({
   language,
-  testimony,
+  // testimony,
   theme,
 });
