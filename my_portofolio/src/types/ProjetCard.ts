fd4699726fb0781898ec87ac7346a7c2b51a4149
DaniloWaffis <danilowaffis@gmail.com>
2024-12-23 18:22:18 +0100

    Utilisation du type Partial

--- my_portofolio/src/types/ProjetCard.ts
+++ my_portofolio/src/types/ProjetCard.ts
@@ -1,9 +1,13 @@
-export interface ProjetCardProps {
-  name?: string;
-  description?: string;
-  tags?: { name: string; color: string }[];
-  image?: string;
-  thumbnail?: string;
-  demo?: string;
-  sourcecode?: string;
+interface ProjetCard {
+  name: string;
+  description: string;
+  tags: { name: string; color: string }[];
+  image: string;
+  thumbnail: string;
+  demo: string;
+  sourcecode: string;
 }
+
+const ProjetCardProps: Partial<ProjetCard> = {};
+
+export default ProjetCardProps;
